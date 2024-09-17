eae57c831a01ce3a7f9be2cb12774a93f1b60c49
ahorn256 <anton.horn@futuristen.education>
2024-09-17 14:09:33 +0200

    updated App routes

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,18 @@ import DeletionDialog from './features/books/DeletionDialog';
 import NotFound from './NotFound';
 import LoginForm from './features/login/LoginForm';
 import Nav from './features/navMenu/Nav';
+import { token } from './apolloClient';
+import { useReactiveVar } from '@apollo/client';
 
 function App() {
+  const serverToken = useReactiveVar(token);
+
   return (
     <div className="App">
       <Nav />
       <div className='App-content'>
         <Routes>
-          <Route path='/' element={<BooksApp />}>
+          <Route path='/' element={ serverToken ? <BooksApp /> : <p>Sie müssen sich anmelden, um den Inhalt zu sehen</p>}>
             <Route path='/new' element={<FormDialog />} />
             <Route path='/edit/:id' element={<FormDialog />} />
             <Route path='/delete/:id' element={<DeletionDialog />} />
